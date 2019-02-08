7ee8f81e2abade7285ccd001a3903682d5d8957a
Ghadi Shayban <gshayban@gmail.com>
2019-02-08 12:50:33 -0500

    initial

---
+++ src/cognitect/monaco/paredit.cljs
@@ -0,0 +1,194 @@
+(ns cognitect.monaco.paredit)
+
+;; integrates
+;; https://robert.kra.hn/past-projects/paredit-js.html
+
+;; TODO
+;; cut/copy/yank/etc
+;; nav expand/contract
+;; wrapAround
+;; indent
+
+;; monaco terms:
+;; editor model selection range position
+;; paredit terms:
+;; ast
+
+(defn rebl-editor
+  []
+  js/window.editor)
+
+(defn paredit-ast
+  [src]
+  (.parse js/paredit src))
+
+(defn offset
+  [model position]
+  (.getOffsetAt model position))
+
+(defn get-selection
+  [editor]
+  (let [model (.getModel editor)
+        sel (.getSelection editor)]
+    {:start (offset model (.getStartPosition sel))
+     :end (offset model (.getEndPosition sel))
+     :cur (offset model (.getPosition sel))}))
+
+(defn apply-edits
+  "takes paredit.js results and applies them to Monaco"
+  [editor pinfo]
+  (when pinfo
+    (let [model (.getModel editor)
+          range (fn [start end]
+                  (.fromPositions js/monaco.Range
+                                  (.getPositionAt model start)
+                                  (.getPositionAt model end)))
+          chgs (map (fn [[op i arg]]
+                      (case op
+                        "insert" #js {:range (range i i)
+                                      :text arg}
+                        "remove" #js {:range (range i (+ i arg))
+                                      :text nil}))
+                    (.-changes pinfo))]
+      (.pushEditOperations model nil (into-array chgs) nil)
+      (.setPosition editor (.getPositionAt model (.-newIndex pinfo))))))
+
+(defn wrap-paredit-command
+  [cmd]
+  (fn [editor]
+    (let [model (.getModel editor)
+          src (.getValue model)
+          pos (.getPosition editor)]
+      (cmd {:editor editor
+            :src src
+            :ast (paredit-ast src)
+            :selection (get-selection editor)}))))
+
+(defn nav-thunk
+  [paredit-cmd]
+  (wrap-paredit-command
+   (fn [{:keys [editor ast src selection]}]
+     (let [model (.getModel editor)
+           nidx (paredit-cmd ast (:cur selection))]
+       (.setPosition editor (.getPositionAt model nidx))))))
+
+(def actions
+  [#js {:id "paredit-slurp-forward"
+        :label "Slurp Forward"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.KEY_0)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.slurpSexp js/paredit.editor ast src (:cur selection) #js {:backward false}))))}
+   #js {:id "paredit-slurp-backward"
+        :label "Slurp Forward"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.KEY_9)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.slurpSexp js/paredit.editor ast src (:cur selection) #js {:backward true}))))}
+
+   #js {:id "paredit-barf-sexp"
+        :label "Barf S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.US_CLOSE_SQUARE_BRACKET)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.barfSexp js/paredit.editor ast src (:cur selection) #js {:backward false}))))}
+
+   #js {:id "paredit-barf-sexp-backward"
+        :label "Barf S-Expression Backwards"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.US_OPEN_SQUARE_BRACKET)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.barfSexp js/paredit.editor ast src (:cur selection) #js {:backward true}))))}
+
+   #js {:id "paredit-kill-sexp"
+        :label "Kill S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyCode.KEY_D)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.killSexp js/paredit.editor ast src (:cur selection) #js {:backward false}))))}
+
+   #js {:id "paredit-kill-sexp-backward"
+        :label "Kill S-Expression Backwards"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.KEY_D)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.killSexp js/paredit.editor ast src (:cur selection) #js {:backward true}))))}
+
+   #js {:id "paredit-split-sexp"
+        :label "Split S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.Alt
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.KEY_S)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.splitSexp js/paredit.editor ast src (:cur selection) #js {}))))}
+
+   #js {:id "paredit-splice-sexp"
+        :label "Splice S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.Alt
+                                  js/monaco.KeyCode.KEY_S)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (apply-edits editor (.spliceSexp js/paredit.editor ast src (:cur selection) #js {}))))}
+
+   #js {:id "paredit-forward"
+        :label "Forward S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Alt
+                                  js/monaco.KeyCode.KEY_F)]
+        :run (nav-thunk js/paredit.navigator.forwardSexp)}
+
+   #js {:id "paredit-backward"
+        :label "Backward S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Alt
+                                  js/monaco.KeyCode.KEY_B)]
+        :run (nav-thunk js/paredit.navigator.backwardSexp)}
+
+   #js {:id "paredit-forward-down"
+        :label "Forward Down S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Alt
+                                  js/monaco.KeyCode.KEY_D)]
+        :run (nav-thunk js/paredit.navigator.forwardDownSexp)}
+
+   #js {:id "paredit-backward-up"
+        :label "Backward Up S-Expression"
+        :keybindings #js [(bit-or js/monaco.KeyMod.WinCtrl
+                                  js/monaco.KeyMod.Alt
+                                  js/monaco.KeyCode.KEY_U)]
+        :run (nav-thunk js/paredit.navigator.backwardUpSexp)}
+
+   ;; not sure what paredit is doing here
+   ;; consider paredit.editor.open() too
+   #_ #js {:id "paredit-wrap-list"
+        :label "Wrap as List"
+        :keybindings #js [(bit-or js/monaco.KeyMod.Alt
+                                  js/monaco.KeyMod.Shift
+                                  js/monaco.KeyCode.KEY_K)]
+        :run (wrap-paredit-command
+              (fn [{:keys [editor ast src selection]}]
+                (let [{:keys [cur start end]} selection]
+                  (apply-edits editor (.wrapAround js/paredit.editor ast src cur "(" ")"
+                                                   (if (= start end)
+                                                     #js {}
+                                                     #js {:endIdx end}))))))}])
+
+(defn ^:export register-actions
+  [editor]
+  (doseq [a actions]
+    (.addAction editor a)))
+
+;; (register-actions (rebl-editor))
+
